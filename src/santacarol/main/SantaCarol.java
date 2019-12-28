52a5c7acf6d84594b00cd77d14fd26251d17f07c
InfernoProgrammer123 <bastivargas@gmail.com>
2019-12-28 10:12:06 -0800

    Finish music!

--- src/santacarol/main/SantaCarol.java
+++ src/santacarol/main/SantaCarol.java
@@ -95,26 +95,83 @@ public class SantaCarol {
 			track.add(makeEvent(true, 1, 64, 154, 100));
 			track.add(makeEvent(true, 1, 66, 156, 100));
 			track.add(makeEvent(true, 1, 62, 158, 100));
-			track.add(makeEvent(true, 1, 71, 161, 100)); // 161
-			track.add(makeEvent(true, 1, 71, 164, 100)); // 164
-			track.add(makeEvent(true, 1, 71, 166, 100)); // 166
-			track.add(makeEvent(true, 1, 69, 168, 100)); // 168
-			track.add(makeEvent(true, 1, 67, 170, 100)); // 170
-			track.add(makeEvent(true, 1, 66, 172, 100)); // 172
-			track.add(makeEvent(true, 1, 66, 175, 100)); // 175
-			track.add(makeEvent(true, 1, 66, 177, 100)); // 177
-			track.add(makeEvent(true, 1, 64, 179, 100)); // 179
-			track.add(makeEvent(true, 1, 62, 181, 100)); // 181
-			track.add(makeEvent(true, 1, 64, 183, 100)); // 183
-			track.add(makeEvent(true, 1, 64, 186, 100)); // 186
-			track.add(makeEvent(true, 1, 64, 188, 100)); // 188
-			track.add(makeEvent(true, 1, 66, 190, 100)); // 190
-			track.add(makeEvent(true, 1, 64, 192, 100)); // 192
-			track.add(makeEvent(true, 1, 59, 194, 100)); // 194
-			track.add(makeEvent(true, 1, 59, 197, 100)); // 197
-			track.add(makeEvent(true, 1, 59, 199, 100)); // 199
-			track.add(makeEvent(true, 1, 59, 201, 100)); // 201
-			
+			track.add(makeEvent(true, 1, 71, 161, 100));
+			track.add(makeEvent(true, 1, 71, 164, 100));
+			track.add(makeEvent(true, 1, 71, 166, 100));
+			track.add(makeEvent(true, 1, 69, 168, 100));
+			track.add(makeEvent(true, 1, 67, 170, 100));
+			track.add(makeEvent(true, 1, 66, 172, 100));
+			track.add(makeEvent(true, 1, 66, 175, 100));
+			track.add(makeEvent(true, 1, 66, 177, 100));
+			track.add(makeEvent(true, 1, 64, 179, 100));
+			track.add(makeEvent(true, 1, 62, 181, 100));
+			track.add(makeEvent(true, 1, 64, 183, 100));
+			track.add(makeEvent(true, 1, 64, 186, 100));
+			track.add(makeEvent(true, 1, 64, 188, 100));
+			track.add(makeEvent(true, 1, 66, 190, 100));
+			track.add(makeEvent(true, 1, 64, 192, 100));
+			track.add(makeEvent(true, 1, 59, 194, 100));
+			track.add(makeEvent(true, 1, 59, 197, 100));
+			track.add(makeEvent(true, 1, 59, 199, 100));
+			track.add(makeEvent(true, 1, 59, 201, 100));
+			track.add(makeEvent(true, 1, 59, 204, 100)); // 204
+			track.add(makeEvent(true, 1, 61, 206, 100)); // 206
+			track.add(makeEvent(true, 1, 63, 208, 100)); // 208
+			track.add(makeEvent(true, 1, 64, 210, 100)); // 210
+			track.add(makeEvent(true, 1, 66, 212, 100)); // 212
+			track.add(makeEvent(true, 1, 67, 214, 100)); // 214
+			track.add(makeEvent(true, 1, 69, 216, 100)); // 216
+			track.add(makeEvent(true, 1, 71, 218, 100)); // 218
+			track.add(makeEvent(true, 1, 69, 220, 100)); // 220
+			track.add(makeEvent(true, 1, 67, 223, 100)); // 223
+			track.add(makeEvent(true, 1, 71, 226, 100)); // 226
+			track.add(makeEvent(true, 1, 73, 228, 100)); // 228
+			track.add(makeEvent(true, 1, 75, 230, 100)); // 230
+			track.add(makeEvent(true, 1, 76, 232, 100)); // 232
+			track.add(makeEvent(true, 1, 78, 234, 100)); // 234
+			track.add(makeEvent(true, 1, 79, 236, 100)); // 236
+			track.add(makeEvent(true, 1, 81, 238, 100)); // 238
+			track.add(makeEvent(true, 1, 83, 240, 100)); // 240
+			track.add(makeEvent(true, 1, 81, 242, 100)); // 242
+			track.add(makeEvent(true, 1, 79, 245, 100)); // 245
+			track.add(makeEvent(true, 1, 79, 248, 100));
+			track.add(makeEvent(true, 1, 78, 251, 100));
+			track.add(makeEvent(true, 1, 79, 253, 100));
+			track.add(makeEvent(true, 1, 76, 255, 100));
+			track.add(makeEvent(true, 1, 79, 248, 100));
+			track.add(makeEvent(true, 1, 78, 251, 100));
+			track.add(makeEvent(true, 1, 79, 253, 100));
+			track.add(makeEvent(true, 1, 76, 255, 100));
+			track.add(makeEvent(true, 1, 79, 258, 100));
+			track.add(makeEvent(true, 1, 78, 261, 100));
+			track.add(makeEvent(true, 1, 79, 263, 100));
+			track.add(makeEvent(true, 1, 76, 265, 100));
+			track.add(makeEvent(true, 1, 79, 268, 100));
+			track.add(makeEvent(true, 1, 78, 271, 100));
+			track.add(makeEvent(true, 1, 79, 273, 100));
+			track.add(makeEvent(true, 1, 76, 275, 100));
+			track.add(makeEvent(true, 1, 79, 278, 100));
+			track.add(makeEvent(true, 1, 78, 281, 100));
+			track.add(makeEvent(true, 1, 79, 283, 100));
+			track.add(makeEvent(true, 1, 76, 285, 100));
+			track.add(makeEvent(true, 1, 79, 288, 100));
+			track.add(makeEvent(true, 1, 78, 291, 100));
+			track.add(makeEvent(true, 1, 79, 293, 100));
+			track.add(makeEvent(true, 1, 76, 295, 100));
+			track.add(makeEvent(true, 1, 79, 298, 100));
+			track.add(makeEvent(true, 1, 78, 301, 100));
+			track.add(makeEvent(true, 1, 79, 303, 100));
+			track.add(makeEvent(true, 1, 76, 305, 100));
+			track.add(makeEvent(true, 1, 79, 308, 100));
+			track.add(makeEvent(true, 1, 78, 311, 100));
+			track.add(makeEvent(true, 1, 79, 313, 100));
+			track.add(makeEvent(true, 1, 76, 315, 100));
+			track.add(makeEvent(true, 1, 79, 318, 100));
+			track.add(makeEvent(true, 1, 78, 321, 100));
+			track.add(makeEvent(true, 1, 79, 323, 100));
+			track.add(makeEvent(true, 1, 76, 325, 100));
+			track.add(makeEvent(false, 1, 76, 341, 100));
+		
 			// Music time! On Channel 2!
 			track.add(makeEvent(true, 2, 47, 41, 120));
 			track.add(makeEvent(true, 2, 45, 51, 120));
@@ -132,6 +189,16 @@ public class SantaCarol {
 			track.add(makeEvent(true, 2, 45, 172, 120));
 			track.add(makeEvent(true, 2, 43, 183, 120));
 			track.add(makeEvent(true, 2, 42, 194, 120));
+			track.add(makeEvent(true, 2, 42, 204, 120));
+			track.add(makeEvent(true, 2, 42, 216, 120));
+			track.add(makeEvent(true, 2, 42, 226, 120));
+			track.add(makeEvent(true, 2, 42, 238, 120));
+			track.add(makeEvent(true, 2, 47, 248, 120));
+			track.add(makeEvent(true, 2, 45, 258, 120));
+			track.add(makeEvent(true, 2, 43, 268, 120));
+			track.add(makeEvent(true, 2, 42, 278, 120));
+			track.add(makeEvent(true, 2, 47, 288, 120));
+			track.add(makeEvent(false, 2, 47, 296, 120));
 			
 			// Let's play the music
 			sequencer.setSequence(seq);
@@ -152,6 +219,7 @@ public class SantaCarol {
 	 * @param channel - Channel on which a note will be played on
 	 * @param note - Note number
 	 * @param beat - Play or releases note at this beat
+	 * @param hardness - How hard the note is being pressed
 	 * @return a MidiEvent to add to a track
 	 */
 	public static MidiEvent makeEvent(boolean noteType, int channel, int note, int beat, int hardness) {
